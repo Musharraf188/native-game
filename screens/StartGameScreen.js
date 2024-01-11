cf76887aeae3f8963ff508dac10e520c213dbc45
Musharraf188 <106909179+Musharraf188@users.noreply.github.com>
2024-01-11 20:34:38 +0530

    Game Screen is Created

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,39 +1,75 @@
-import { View, TextInput, StyleSheet } from 'react-native';
-import PrimaryButton from '../componets/PrimaryButton';
+import { View, TextInput, StyleSheet, Alert } from "react-native";
+import { useState } from "react";
+import PrimaryButton from "../componets/PrimaryButton";
 
-function StartGameScreen() {
-    return <View style={styles.inputContainer}>
-        <View style={styles.inputFiled}>
-        <TextInput  style={styles.inputNummber} maxLength={2} keyboardType="number-pad" />
+function StartGameScreen({onPickedNumber}){
+  const [enteredNumber, setEnteredNumber] = useState("");
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+  function resetInputNumber(){
+    setEnteredNumber('');
+  }
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber); // convert to number
+    if (isNaN(enteredNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+        // Show alert..
+Alert.alert('Invalid Number!',
+'Number has to be a number between 1 and 99.',
+[{text: 'Okay', style  : 'destructive', onPress: resetInputNumber }]
+)
+      return;
+    }
+    onPickedNumber(chosenNumber);
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <View style={styles.inputFiled}>
+        <TextInput
+          style={styles.inputNummber}
+          maxLength={2}
+          keyboardType="number-pad"
+          onChangeText={numberInputHandler}
+          value={enteredNumber}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputNumber}>Rest</PrimaryButton>
         </View>
-        <PrimaryButton>Rest</PrimaryButton>
-        <PrimaryButton>Conform</PrimaryButton>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Conform</PrimaryButton>
+        </View>
+      </View>
     </View>
+  );
 }
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginHorizontal: 24,
-        borderRadius: 8,
-        marginTop: 100,
-        padding: 16,
-        backgroundColor: '#4e0329',
-        elevation: 17
-    },
-    inputNummber: {
-        height: 50,
-        width :50,
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight : 'bold',
-        textAlign : 'center'
-    
-    },
-    inputFiled :{
-        textAlign : 'center'
-    }
+  inputContainer: {
+    alignItems: "center",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#3b021f",
+    elevation: 17,
+  },
+  inputNummber: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 });
-export default StartGameScreen
-
+export default StartGameScreen;
